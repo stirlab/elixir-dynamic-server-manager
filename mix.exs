d0194d7a9bd46e5341aa984e8b64253887e64a14
Chad Phillips <chad@apartmentlines.com>
2018-04-27 18:49:30 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DynamicServerManager.Mixfile do
   def project do
     [
       app: :dynamic_server_manager,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       package: package(),
